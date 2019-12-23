2ebe36e17a47213c20a0445272e49c7d424e53ad
Tsadkiel1612 <edilsonsmj@yahoo.com.br>
2019-12-23 14:10:34 -0300

    Iniciando a implementacao das Pecas e do Tabuleiro

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,4 +1,13 @@
 package application;
 
+import boardgame.Posicao;
+import boardgame.Tabuleiro;
+
 public class Programa {
+
+    public static void main(String[] args) {
+	// write your code here
+        Tabuleiro tabuleiro = new Tabuleiro(8,8);
+        
+    }
 }
